7ce9466c5c0b8187229a5b260d7e638641ef6025
Sven Koehler <sven.koehler@hpi.de>
2021-02-18 23:49:26 +0100

    Layout url scheme

--- tenca_webui.py
+++ tenca_webui.py
@@ -1,6 +1,28 @@
+from markupsafe import escape
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'It works!'
+    return 'This is the main page! Here you can only log-in'
+
+@app.route('/confirm/<list_id>/<token>')
+def confirm_action(list_id, token):
+	return 'So, you want to confirm your action "%s" on list "%s"?' % (escape(list_id), escape(token))
+
+@app.route('/report/<list_id>/<token>')
+def report_action(list_id, token):
+	return 'I will log your complain with "%s" on list "%s"?' % (escape(list_id), escape(token))
+
+@app.route('/manage/<list_id>')
+def manage_list(list_id):
+    return 'This is the admin view for the owners of "%s" (requires login)!' % escape(list_id)
+
+@app.route('/dashboard/<username>')
+def user_dashboard(username):
+    return 'This shows the lists "%s" is owner or member of (requires login)!' % escape(username)
+
+@app.route('/<hashid>')
+def subscribe_list(hashid):
+	return 'Displaying (un)-subscription form of "%s"' % escape(hashid)
