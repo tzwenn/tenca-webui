11ed1f583e5e8330f2996968addf8026f708f7d8
Sven Koehler <sven.koehler@hpi.de>
2021-02-19 10:10:40 +0100

    Implement confirmation and report action

--- tenca_webui.py
+++ tenca_webui.py
@@ -1,19 +1,67 @@
+import logging
+
 from markupsafe import escape
 from flask import Flask
 
+import tenca.connection
+import tenca.settings
+import tenca.exceptions
+
+
 app = Flask(__name__)
+logger = logging.Logger(__name__)
+
+conn = tenca.connection.Connection()
+
+
+################################################################################
+
+def lookup_list_and_email_by_action(list_id, token):
+	mailing_list = conn.get_list(list_id)
+	try:
+		email = mailing_list.pending_subscriptions()[token]
+	except KeyError:
+		# As of mailman<3.3.3 no unsubscriptions are exposed via
+		# the REST API. This exception might be quite misleading.
+		email = None
+	return mailing_list, email
+
+
+################################################################################
+
 
 @app.route('/')
 def index():
-    return 'This is the main page! Here you can only log-in'
+    return 'This is the main page! Here you can only log-in.'
 
 @app.route('/confirm/<list_id>/<token>')
 def confirm_action(list_id, token):
-	return 'So, you want to confirm your action "%s" on list "%s"?' % (escape(list_id), escape(token))
+	mailing_list, email = lookup_list_and_email_by_action(escape(list_id), escape(token))
+	
+	try:
+		mailing_list.confirm_subscription(escape(token))
+	except tenca.exceptions.NoSuchRequestException:
+		return 'Unknown action'     # TODO: better 404?
+
+	if email is not None:
+		conn.mark_address_verified(email)
+		return 'Hi "%s", you successfully joined "%s"' % (email, mailing_list.fqdn_listname)
+	else:
+		return "ok. thx. bye."
 
 @app.route('/report/<list_id>/<token>')
 def report_action(list_id, token):
-	return 'I will log your complain with "%s" on list "%s"?' % (escape(list_id), escape(token))
+	mailing_list, email = lookup_list_and_email_by_action(escape(list_id), escape(token))
+	
+	logger.warn('Report received for list "{}" and token "{}" (address "{}")'.format(
+		escape(list_id), escape(token), email
+	))
+	try:
+		mailing_list.cancel_pending_subscription(escape(token))
+	except tenca.exceptions.NoSuchRequestException:
+		pass
+
+	return 'Your report has been logged. Thank you.'
 
 @app.route('/manage/<list_id>')
 def manage_list(list_id):
