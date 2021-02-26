3b3ce77cb0ef3cb97302f9a59afc1c85b9c54571
Sven Koehler <sven.koehler@hpi.de>
2021-02-26 19:29:13 +0100

    Move actions to seperate blueprint

--- tenca_webui/__init__.py
+++ tenca_webui/__init__.py
@@ -46,18 +46,6 @@ def create_app(test_config=None):
 
 	################################################################################
 
-	def lookup_list_and_email_by_action(list_id, token):
-		mailing_list = conn.get_list(list_id)
-		try:
-			email = mailing_list.pending_subscriptions()[token]
-		except KeyError:
-			# As of mailman<3.3.3 no unsubscriptions are exposed via
-			# the REST API. This exception might be quite misleading.
-			email = None
-		return mailing_list, email
-
-	################################################################################
-
 	@app.route('/')
 	def index():
 		if oidc.user_loggedin:
@@ -65,34 +53,8 @@ def create_app(test_config=None):
 		else:
 			return render_template('index.html')
 
-	@app.route('/confirm/<list_id>/<token>/')
-	def confirm_action(list_id, token):
-		mailing_list, email = lookup_list_and_email_by_action(escape(list_id), escape(token))
-
-		try:
-			mailing_list.confirm_subscription(escape(token))
-		except tenca.exceptions.NoSuchRequestException:
-			return 'Unknown action'     # TODO: better 404?
-
-		if email is not None:
-			conn.mark_address_verified(email)
-			return 'Hi "%s", you successfully joined "%s"' % (email, mailing_list.fqdn_listname)
-		else:
-			return "ok. thx. bye."
-
-	@app.route('/report/<list_id>/<token>/')
-	def report_action(list_id, token):
-		mailing_list, email = lookup_list_and_email_by_action(escape(list_id), escape(token))
-		
-		app.logger.warn('Report received for list "{}" and token "{}" (address "{}")'.format(
-			escape(list_id), escape(token), email
-		))
-		try:
-			mailing_list.cancel_pending_subscription(escape(token))
-		except tenca.exceptions.NoSuchRequestException:
-			pass
-
-		return 'Your report has been logged. Thank you.'
+	from . import action
+	app.register_blueprint(action.bp)
 
 	@app.route('/manage/<list_id>/')
 	@oidc.require_login
---
+++ tenca_webui/action.py
@@ -0,0 +1,51 @@
+from flask import Blueprint, Markup, abort, current_app, escape, flash, g, render_template
+
+import tenca.exceptions
+
+bp = Blueprint('action', __name__)
+
+def lookup_list_and_email_by_action(list_id, token):
+	mailing_list = g.conn.get_list(list_id)
+	if mailing_list is None:
+		abort(404)
+	try:
+		email = mailing_list.pending_subscriptions()[token]
+	except KeyError:
+		# As of mailman<3.3.3 no unsubscriptions are exposed via
+		# the REST API. This exception might be quite misleading.
+		email = None
+	return mailing_list, email
+
+@bp.route('/confirm/<list_id>/<token>/')
+def confirm_action(list_id, token):
+	mailing_list, email = lookup_list_and_email_by_action(escape(list_id), escape(token))
+
+	try:
+		mailing_list.confirm_subscription(escape(token))
+	except tenca.exceptions.NoSuchRequestException:
+		abort(404)
+
+	if email is not None:
+		g.conn.mark_address_verified(email)
+		flash(Markup('{} has successfully joined <strong>{}</strong>.'.format(email, mailing_list.fqdn_listname)), category='success')
+	else:
+		flash(Markup('Ok. Thx. Bye from <strong>{}</strong>.'.format(mailing_list.fqdn_listname)), category='success')
+	
+	return render_template('action.html')
+
+@bp.route('/report/<list_id>/<token>/')
+def report_action(list_id, token):
+	mailing_list, email = lookup_list_and_email_by_action(escape(list_id), escape(token))
+
+	current_app.logger.warn('Report received for list "{}" and token "{}" (address "{}")'.format(
+		escape(list_id), escape(token), email
+	))
+	try:
+		mailing_list.cancel_pending_subscription(escape(token))
+	except tenca.exceptions.NoSuchRequestException:
+		pass # We don't tell.
+
+	flash('Your report has been logged. Thank you.'.format(mailing_list.fqdn_listname), category='success')
+
+	return render_template('action.html')
+
