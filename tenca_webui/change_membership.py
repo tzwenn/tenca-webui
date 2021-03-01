46ee4c874b5f81b083fd73f404a3418e08853e44
Sven Koehler <sven.koehler@hpi.de>
2021-03-01 23:05:35 +0100

    Properly complain, when the last owner leaves the list

--- tenca_webui/change_membership.py
+++ tenca_webui/change_membership.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, current_app, g, render_template, request
+from flask import Blueprint, Markup, abort, current_app, flash, g, render_template, request
 from markupsafe import escape
 from wtforms import Form, validators
 from wtforms.fields.html5 import EmailField
@@ -20,12 +20,16 @@ def index(hash_id):
 	form = SubscriptionForm(request.form)
 
 	if request.method == 'POST' and form.validate():
-		joined, token = mailing_list.toggle_membership(form.email.data)
-		current_app.logger.info('Received {} request for "{}", with token {}.'.format(
-			'subscription' if joined else 'unsubscription',
-			form.email.data, token
-		))
+		try:
+			joined, token = mailing_list.toggle_membership(form.email.data)
+		except Exception as e:
+			flash(Markup("An Error occurred: {}".format(escape(str(e)))), category="danger");
+		else:
+			current_app.logger.info('Received {} request for "{}", with token {}.'.format(
+				'subscription' if joined else 'unsubscription',
+				form.email.data, token
+			))
 
-		return render_template('change_membership/finished.html')
+			return render_template('change_membership/finished.html')
 
 	return render_template('change_membership/index.html', form=form, mailing_list=mailing_list)
